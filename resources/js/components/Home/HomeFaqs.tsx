c05c0cdb967c598ee9943ef623a162d980eb1f47
Afifi Mac <aabdallahart@gmail.com>
2025-10-03 03:36:30 +0300

    fix faqs

--- resources/js/components/Home/HomeFaqs.tsx
+++ resources/js/components/Home/HomeFaqs.tsx
@@ -7,7 +7,7 @@ export default function HomeFaqs() {
     return (
         <section className="flex gap-6 sm:gap-8 md:gap-10 py-12 sm:py-16 md:py-24 px-4 sm:px-6 md:px-10 bg-ui-1 items-start justify-center">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 sm:gap-8 md:gap-6 max-w-6xl w-full">
-                <div className="sticky top-24 lg:col-span-1 flex flex-col gap-4 sm:gap-6 self-start">
+                <div className="md:sticky md:top-24 lg:col-span-1 flex flex-col gap-4 sm:gap-6 self-start">
                     <Tag text="FAQ" />
                     <div className="flex flex-col gap-4 sm:gap-6">
                         <Text variant="heading2" className="text-text-primary w-full">Your questions answered</Text>
