b3f94bdf4348a13818a469e0d929c1b8cfe4dde2
Afifi Mac <aabdallahart@gmail.com>
2025-10-03 03:33:16 +0300

    update the build

--- public/build/assets/app-layout-BzYT7uBE.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-BJ_dQBFq.js";import{H as r,F as a}from"./Header-y03RxarN.js";function n({children:s,color:t="white",section:o="nav"}){return e.jsxs("main",{className:"flex flex-col items-center",children:[e.jsx(r,{section:o,color:t}),s,e.jsx(a,{})]})}const m=({children:s,...t})=>e.jsx(n,{...t,children:s});export{m as A};
